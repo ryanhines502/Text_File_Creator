18499621a54a0c3eb2b100f757fee16cd3927325
Ryan Hines <ryanhines502@gmail.com>
2026-02-16 17:34:22 -0500

    First Commit

---
+++ Projects/Text_File_Creator/New Python Source File.py
@@ -0,0 +1,6 @@
+name = input("What's your name? \n")
+color = input("What's your favorite color? \n")
+with open('example.txt', 'w') as file:
+    file.write(f"{name} created this file")
+    file.write(f"{color} is their favorite color")
+print(f"{name}, {color}")
